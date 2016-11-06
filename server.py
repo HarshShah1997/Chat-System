4def2325d34951f5b9bd19b33721c07b171ec241
Harsh Shah <harsh.shah.jayesh@gmail.com>
2016-11-06 23:43:45 +0530

    Added basic website

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
 from flask import *
 
 app = Flask(__name__)
+messages = []
 
 @app.route("/")
 def root():
-    return "Initial commit"
+    return render_template("home.html", messages=messages)
+
+@app.route("/receiveMessage", methods=["POST"])
+def receiveMessage():
+    message = request.form['chatText']
+    messages.append(message)
+    return redirect(url_for('root'))
 
 if __name__ == '__main__':
     app.run(debug=True)
