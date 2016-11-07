afa9d092a136590e43bcbec7003b4577a8e4c17f
Harsh Shah <harsh.shah.jayesh@gmail.com>
2016-11-07 17:32:11 +0530

    Sending messages with ajax

--- server.py
+++ server.py
@@ -7,11 +7,12 @@ messages = []
 def root():
     return render_template("home.html", messages=messages)
 
-@app.route("/receiveMessage", methods=["POST"])
+@app.route("/_receiveMessage", methods=["POST"])
 def receiveMessage():
     message = request.form['chatText']
     messages.append(message)
-    return redirect(url_for('root'))
+    return message
+    #return redirect(url_for('root'))
 
 if __name__ == '__main__':
     app.run(debug=True)
