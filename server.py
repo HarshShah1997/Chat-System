9972da29c15146a91363dfff5e09b5e1a826ccdd
Harsh Shah <harsh.shah.jayesh@gmail.com>
2016-11-11 10:57:52 +0530

    Starting upload files

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 from flask import *
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = 'static/uploads'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 messages = []
 
 @app.route("/")
@@ -20,12 +25,28 @@ def sendMessagesList():
     rendered = getHtml()
     return rendered
 
+@app.route("/_uploadFile", methods=["POST"])
+def uploadFile():
+    recvFile = request.files['file']
+    if recvFile.filename == '':
+        return 'No file selected'
+
+    allowed_extensions = set(['txt', 'pdf', 'doc', 'docx', 'ppt', 'pptx'])
+
+    if recvFile and allowed_file(recvFile.filename, allowed_extensions):
+        filename = secure_filename(recvFile.filename)
+        recvFile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'Uploaded successfully'
+    return 'Invalid file'
+
 def getHtml():
     text = '''{% for name, msg in messages %}
                 {{ name }}: {{ msg }}<br>
               {% endfor %}'''
     return render_template_string(text, messages=messages)
 
+def allowed_file(filename, allowed_extensions):
+    return '.' in filename and filename.rsplit('.', 1)[1] in allowed_extensions
 
 if __name__ == '__main__':
     app.run(debug=True)
